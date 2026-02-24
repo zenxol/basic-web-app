050f6490f3bd5f0c0114a7ff838e2ffc5e438a51
zenxol <aiden.nguyen101@gmail.com>
2026-02-24 14:55:21 +0000

    changed name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Aiden Nguyen";
   }
 
   if (query.toLowerCase().includes("andrew id")) {
